a069837488ffbedbe23fbc6cce7fac3b8fac050c
ylitvyakov <ylitvyakov@yahoo.com>
2019-07-14 12:44:57 +0300

    Add stats output after testing is finished

--- src/subpages/Tests.js
+++ src/subpages/Tests.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import * as api from '../api';
 import { FaPlay as Run } from 'react-icons/fa';
 import socketIOClient from 'socket.io-client';
+import moment from 'moment';
 
 class Tests extends React.Component {
   state = {
@@ -16,37 +17,43 @@ class Tests extends React.Component {
 
   async componentDidMount() {
     const filters = {
-      type: _.get(this.props, 'match.params.type', 'func'),
+      type: this.type,
     };
     const tests = await api.fetchTests(filters);
 
-    this.setState({ tests });
+    this.setState({
+      tests,
+      stats: JSON.parse(localStorage.getItem(this.type === 'func' ? 'func_stats' : 'load_stats')),
+      log: localStorage.getItem(this.type === 'func' ? 'func_log' : 'load_log'),
+    });
 
     const socket = socketIOClient('http://localhost:3000');
 
     socket.on('tests:end', (stats) => {
-      console.log(stats);
       this.setState({ loading: false, stats });
+      localStorage.setItem(this.type === 'func' ? 'func_stats' : 'load_stats', JSON.stringify(stats));
     });
 
     socket.on('tests:log', (log) => {
-      this.setState({
-        log: this.state.log + log,
-      });
+      this.setState({ log: this.state.log + log });
+      localStorage.setItem(this.type === 'func' ? 'func_log' : 'load_log', this.state.log + log);
     });
   }
 
   async componentDidUpdate(prevProps) {
-    const { type } = this.props.match.params;
     const { type: prevType } = prevProps.match.params;
 
-    if (type !== prevType) {
+    if (this.type !== prevType) {
       const filters = {
-        type,
+        type: this.type,
       };
       const tests = await api.fetchTests(filters);
-
-      this.setState({ tests, selected: [] });
+      this.setState({
+        tests,
+        selected: [],
+        stats: JSON.parse(localStorage.getItem(this.type === 'func' ? 'func_stats' : 'load_stats')),
+        log: localStorage.getItem(this.type === 'func' ? 'func_log' : 'load_log'),
+      });
     }
   }
 
@@ -67,9 +74,10 @@ class Tests extends React.Component {
   };
 
   handleRun = async () => {
+    const selectedTests = _.get(this.state, 'selected', this.state.tests);
     const filters = {
-      type: _.get(this.props, 'match.params.type', 'func'),
-      tests: this.state.selected,
+      type: this.type,
+      tests: selectedTests.map(test => test.id),
     };
 
     this.setState({
@@ -80,8 +88,72 @@ class Tests extends React.Component {
     await api.runTests(filters);
   };
 
+  get type() {
+    return _.get(this.props, 'match.params.type', 'func');
+  }
+
   createMarkup = () => {
     return { __html: this.state.log };
+  };
+
+  getCodeColor = (code) => {
+    switch (true) {
+      case code >= 200 && code < 300:
+        return 'success';
+      case code >= 300 && code < 400:
+        return 'primary';
+      default:
+        return 'danger';
+    }
+  };
+
+  renderCodes = () => {
+    const codes = _.get(this.state, 'stats.aggregate.codes', {});
+
+    return Object.keys(codes).map(code => (
+      <Badge className="m-1" variant={this.getCodeColor(code)}>{code}: {codes[code]}</Badge>
+    ));
+  };
+
+  renderScenarios = () => {
+    const scenarios = _.get(this.state, 'stats.aggregate.scenarioCounts', {});
+
+    return Object.keys(scenarios).map(scenario => (
+      <Badge className="m-1" variant="light">{scenario}: {scenarios[scenario]}</Badge>
+    ));
+  };
+
+  renderLatency = () => {
+    const latencies = _.get(this.state, 'stats.aggregate.latency', {});
+
+    return Object.keys(latencies).map(latency => (
+      <Badge className="m-1" variant="light">{latency}: {latencies[latency]}</Badge>
+    ));
+  };
+
+  renderStats = () => {
+    if (this.type === 'func')
+      return (
+        <span className="ml-auto">
+          <small className="m-1"><b>Started at:</b> {moment(_.get(this.state, 'stats.start')).format('DD.MM.YY hh:mm:ss')}</small>
+          <small className="m-1"><b>Finished at:</b> {moment(_.get(this.state, 'stats.end')).format('DD.MM.YY hh:mm:ss')}</small>
+          <small className="m-1"><b>Duration:</b> {_.get(this.state, 'stats.duration')}</small>
+          <Badge className="m-1" variant="light">Tests: {_.get(this.state, 'stats.tests', 0)}</Badge>
+          <Badge className="m-1" variant="success">Passed: {_.get(this.state, 'stats.passes', 0)}</Badge>
+          <Badge className="m-1" variant="danger">Failed: {_.get(this.state, 'stats.failures', 0)}</Badge>
+        </span>
+      );
+
+    return (
+      <span className="ml-auto">
+        <small className="m-1">Codes:</small>
+        {this.renderCodes()}
+        <small className="m-1">Scenarios:</small>
+        {this.renderScenarios()}
+        <small className="m-1">Latency:</small>
+        {this.renderLatency()}
+      </span>
+    );
   };
 
   render() {
@@ -104,11 +176,7 @@ class Tests extends React.Component {
               <ProgressBar now={0} className="my-auto" />
             </Col>
           ) : (
-            <span className="ml-auto">
-              <Badge className="m-1" variant="light">Tests: {_.get(this.state, 'stats.tests', 0)}</Badge>
-              <Badge className="m-1" variant="success">Passed: {_.get(this.state, 'stats.passes', 0)}</Badge>
-              <Badge className="m-1" variant="danger">Failed: {_.get(this.state, 'stats.fails', 0)}</Badge>
-            </span>
+            this.renderStats()
           )}
         </Row>
         <Row className="my-3">
