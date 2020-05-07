85cd34ac2fa6a2cdf2cfd2d85d38db6c1f219681
ylitvyakov <ylitvyakov@gmail.com>
2020-05-07 16:48:49 +0300

    Hotfix v0.2.14:
    - Mark config file with unique key
    - Remove unnecessary 'ms' from message template

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 nconf.argv()
   .env()
-  .file(({ file: path.join(process.cwd(), 'config.json') }));
+  .file('tasty-ui', { file: path.join(process.cwd(), 'config.json') });
 
 module.exports = nconf;
