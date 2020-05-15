1723783da7289c3162373fd4e05fb3feed56ca39
darialutkova <daria.lutkova@leroymerlin.ru>
2020-05-15 13:18:02 +0300

    Close Issue #11: Implement Progress Bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 import Testing from './pages/Testing';
 import Reports from './pages/Reports';
 
-const PROJECT_NAME = 'Lego';
+import { getProjectName } from "./api";
 
 function App() {
+  const [ProjectName, setProjectName] = useState('');
+
+  useEffect( () => {
+    (async () => {
+      const res = await getProjectName();
+      setProjectName(res.name)
+    })()
+}, []);
+
   return (
     <Router>
       <Navbar variant="dark" bg="dark" expand="lg">
-        <Navbar.Brand as={NavLink} to="/">{`${PROJECT_NAME} Tasty Point`}</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">{`${ProjectName} Tasty Point`}</Navbar.Brand>
         <Nav className="mr-auto">
           <NavLink to="/tests" className="nav-link">Tests</NavLink>
           <NavLink to="/reports" className="nav-link">Reports</NavLink>
